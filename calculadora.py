12ca7848101fdc1a7707f9f989e991187008505d
Ignacio Avilés <88689569+avilesxd@users.noreply.github.com>
2022-06-07 11:19:37 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,6 @@
+import time
+
+
 def suma(num1, num2):
     return num1 + num2
 
@@ -6,16 +9,23 @@ def resta(num1, num2):
     return num1 - num2
 
 
-def multiplicacion(num1, num2):
-    return num1 * num2
+def tabla_multiplicacion():
+    num1 = int(input("Ingrese la tabla que desee saber: "))
+    print("\n")
+    for i in range(1, 13):
+        print(f"{num1} x {i} = {num1 * i}")
+    print("\n")
 
 
 def division(num1, num2):
     return num1 / num2
 
 
-def potencia(num1, num2):
-    return num1 ** num2
+def potenciatotal():
+    base = int(input("Ingrese el numero base: "))
+    print("\n")
+    for i in range(1, 13):
+        print(f"{base} ^ {i} = {base ** i}")
 
 
 def raiz(num1):
@@ -56,10 +66,7 @@ def main():
             print(f"La resta de {num1} y {num2} es {resta(num1, num2)}")
 
         elif opcion == 3:
-            num1 = float(input("Escribe el primer número: "))
-            num2 = float(input("Escribe el segundo número: "))
-            print(
-                f"La multiplicación de {num1} y {num2} es {multiplicacion(num1, num2)}")
+            tabla_multiplicacion()
 
         elif opcion == 4:
             num1 = float(input("Escribe el dividendo: "))
@@ -70,17 +77,17 @@ Escribe otro número: """))
             print(f"La división de {num1} y {num2} es {division(num1, num2)}")
 
         elif opcion == 5:
-            num1 = float(input("Escribe el número base: "))
-            num2 = float(input("Escribe el exponente: "))
-            print(
-                f"La potencia de {num1} elevado a {num2} es {potencia(num1, num2)}")
+            potenciatotal()
 
         elif opcion == 6:
             num1 = float(input("Escribe el número: "))
             print(f"La raíz de {num1} es {raiz(num1)}")
 
         elif opcion == 7:
-            print("Adiós, hasta pronto")
+            print("Saliendo...")
+            time.sleep(2)
+            print("Hasta pronto!")
+            time.sleep(1)
             break
 
 
